42e1e6f179f58f527f377cc6b9fdfea88aa65521
Angelica <angelicabbate@gmail.com>
2023-09-14 14:18:31 -0300

    Create a custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,33 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { AxiosError } from "axios";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGames {
-  count: number;
-  results: Game[];
-}
+import useGames, { Game } from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const allUsers = async () => {
-      try {
-        const res = await apiClient.get<FetchGames>("/games");
-        setGames(res.data.results);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-    };
-    allUsers();
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,36 @@
+import { AxiosError, CanceledError } from "axios";
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+
+export interface Game {
+    id: number;
+    name: string;
+  }
+  
+export interface FetchGames {
+    count: number;
+    results: Game[];
+  }
+
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+  
+    useEffect(() => {
+    const controller = new AbortController();
+      const allUsers = async () => {
+        try {
+          const res = await apiClient.get<FetchGames>("/games", {signal: controller.signal});
+          setGames(res.data.results);
+        } catch (err) {
+            if (err instanceof CanceledError) return;
+          setError((err as AxiosError).message);
+        }
+        controller.abort();
+      };
+      allUsers();
+    }, []);
+    return {games, error};
+}
+
+export default useGames;
