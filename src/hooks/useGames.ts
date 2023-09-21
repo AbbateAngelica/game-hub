0f27b3935bb76809787139713903978b194bfe33
Angelica <angelicabbate@gmail.com>
2023-09-20 22:05:41 -0300

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,12 +1,13 @@
 import { GameQuery } from "../App";
-import useData from "./UseData";
+import useData from "./useData";
 
 export interface Game {
     id: number;
     name: string;
     background_image: string; 
-    parent_platforms?: { platform: Platform}[]
+    parent_platforms?: { platform: Platform}[],
     metacritic: number,
+    rating_top: number,
   }
   
 export interface Platform {
