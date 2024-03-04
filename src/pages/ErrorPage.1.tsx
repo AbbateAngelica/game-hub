cbeba6d706358f37da0f490e0a029efe5dc3e17d
Angelica <angelicabbate@gmail.com>
2024-03-03 21:37:26 -0300

    Handle errors

---
+++ src/pages/ErrorPage.1.tsx
@@ -0,0 +1,17 @@
+import { Heading } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+export const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <Heading>Oops...</Heading>
+
+      <Text>
+        {isRouteErrorResponse(error)
+          ? "This page does not exist."
+          : "Sorry, an unexpected error has occurred."}
+      </Text>
+    </>
+  );
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,21 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Homepage from "./pages/HomePage";
 import GameDetailPage from "./pages/GameDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
-    path: "/gamehub-project/",
-    element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
-      { index: true, element: <Homepage></Homepage> },
-      { path: "games/:id", element: <GameDetailPage></GameDetailPage> },
+      {
+        path: "/gamehub-project/",
+        element: <Layout></Layout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+          { index: true, element: <Homepage></Homepage> },
+          { path: "games/:id", element: <GameDetailPage></GameDetailPage> },
+        ],
+      },
     ],
   },
 ]);
